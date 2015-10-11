95a7142a38a14126dfc2e8060b5d829dc2b470f7
Murali Krishna <vmurali1444@gmail.com>
2015-10-11 21:54:45 +0530

    Frontend | added post a new todo

--- public/todoApp.js
+++ public/todoApp.js
@@ -1,14 +1,31 @@
 var app = angular.module("TodoApp", []);
 
-app.controller("todoController", function($http) {
+app.controller("todoController", function ($http) {
     var todos = this;
+    todos.newTodo = {};
+
     $http.get('/api/todos')
-        .success(function(data, status){
-            if(status === 200){
+        .success(function (data, status) {
+            if (status === 200) {
                 todos.list = data;
             }
         })
-        .error(function(data, status){
+        .error(function (data, status) {
             console.log(data);
         });
+
+    todos.addTodo = function () {
+        $http.post('/api/todos', todos.newTodo)
+            .success(function (data, status) {
+                if (status === 200) {
+                    todos.list.push({
+                        "text": todos.newTodo.text,
+                        "done": false
+                    });
+                }
+            })
+            .error(function (data, status) {
+                console.log(data);
+            });
+    }
 });
