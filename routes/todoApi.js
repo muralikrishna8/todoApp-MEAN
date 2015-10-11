fe4516491e6e2acd9443f730172cf5fa2b326b10
Murali Krishna <vmurali1444@gmail.com>
2015-10-11 21:20:10 +0530

    Backend | Added post a todo in api

--- routes/todoApi.js
+++ routes/todoApi.js
@@ -10,4 +10,15 @@ router.get("/", function(req, res){
     });
 });
 
+router.post("/", function(req, res){
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function(err, todo){
+        if(err)
+            res.send(err);
+        res.json(todo);
+    })
+});
+
 module.exports = router;
